ab64d194036ce6b117ef385840b4979623dea120
Ben Izadi <behnam.iam@gmail.com>
2020-04-21 10:22:03 +0100

    new header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ const HatsPage = () => (
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route path='/shop' component={ShopPage} />
