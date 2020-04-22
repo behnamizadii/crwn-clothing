59e22f6df0886983e8a3c6c5bef2b7bebfa2b9b4
Unknown <behnam.iam@gmail.com>
2020-04-22 10:06:27 +0100

    added sign in component with custom button and fields

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
 
 import './App.css';
-
+import SignInAndSignUp from './pages/sing-in-and-sign-up/sing-in-and-sign-up.component';
 import HomePage from './pages/homepage/homepage.component';
 
 const HatsPage = () => (
@@ -20,6 +20,7 @@ function App() {
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route path='/shop' component={ShopPage} />
+				<Route path='/signin' component={SignInAndSignUp} />
 			</Switch>
 		</div>
 	);
