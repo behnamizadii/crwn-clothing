ee86e9aa474352fb9a23e0989fc3c0d47c28f19b
Ben Izadi <behnam.iam@gmail.com>
2020-04-26 00:14:57 +0100

    Sign up procedure with google firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component';
 
 import './App.css';
-import SignInAndSignUp from './pages/sing-in-and-sign-up/sing-in-and-sign-up.component';
-import HomePage from './pages/homepage/homepage.component';
-import { auth } from './firebase/firebase.utils';
 
-const HatsPage = () => (
-	<div>
-		<h1>HATS PAGE </h1>
-	</div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sing-in-and-sign-up/sing-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+	constructor() {
+		super();
 
 		this.state = {
 			currentUser: null
@@ -26,8 +21,23 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		auth.onAuthStateChanged((user) => {
-			this.setState({ currentUser: user });
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot((snapShot) => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					});
+
+					console.log(this.state);
+				});
+			}
+
+			this.setState({ currentUser: userAuth });
 		});
 	}
 
@@ -42,7 +52,7 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route path='/shop' component={ShopPage} />
-					<Route path='/signin' component={SignInAndSignUp} />
+					<Route path='/signin' component={SignInAndSignUpPage} />
 				</Switch>
 			</div>
 		);
